5a74c01c055bbf819017dd4610316f60415ce30f
pp-hibithx <nemdal17@gmail.com>
2026-08-28 16:12:16 +0900

    Add files via upload

--- supabase/functions/share-preview/index.ts
+++ supabase/functions/share-preview/index.ts
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SITE_BASE_URL = (Deno.env.get("SITE_BASE_URL") || "https://pp-hibithx.github.io/39-2").replace(/\/$/, "");
+const SITE_BASE_URL = (Deno.env.get("SITE_BASE_URL") || "https://pp-hibithx.github.io/sakumeru").replace(/\/$/, "");
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL") || "";
 const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") || "";
 const SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") || "";
